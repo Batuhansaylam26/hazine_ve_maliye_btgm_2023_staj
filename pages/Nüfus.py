15045b19620c7509c703f19ce85ebd7f4885ef87
batuhansaylam26 <batuhansaylam1881@gmail.com>
2025-05-26 22:39:54 +0300

    finish

--- pages/Nüfus.py
+++ pages/Nüfus.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html, dcc,Input,Output,callback
+from dash import html, dcc, Input, Output, callback
 import pandas
 import plotly.express as px
 import plotly.graph_objects as go
@@ -10,166 +10,231 @@ import numpy as np
 import plotly.figure_factory as ff
 from sklearn.linear_model import LinearRegression
 from plotly.subplots import make_subplots
-load_figure_template("slate")
-data=pandas.read_csv("/home/batuhansaylam/Desktop/hazine_ve_maliye_btgm_2023_staj/data/soci_econ_country_profiles.csv")
-data1=pandas.read_csv("/home/batuhansaylam/Desktop/hazine_ve_maliye_btgm_2023_staj/data/HDR21-22_Composite_indices_complete_time_series.csv")
-data=data.merge(data1[["iso3","country"]],how="inner",on="country")
-dash.register_page(__name__, path='/population')
 
-data2=data[["country","Region","Population, male"]]
-data2=data2.groupby(["Region"])["Population, male"].sum().reset_index()
-data2=pandas.DataFrame(data2)
-data2["gender"]=["male"]*len(data2)
-data2.columns=["Region","count","gender"]
-data2=data2.sort_values(by=["Region"])
-data3=data[["country","Region","Population, female"]]
-data3=data3.groupby(["Region"])["Population, female"].sum().reset_index()
-data3=pandas.DataFrame(data3)
-data3["gender"]=["female"]*len(data3)
-data3.columns=["Region","count","gender"]
-data3=data3.sort_values(by=["Region"])
-data2=pandas.concat([data2,data3])
+load_figure_template("slate")
+data = pandas.read_csv(
+    "/home/batuhansaylam/Desktop/hazine_ve_maliye_btgm_2023_staj/data/soci_econ_country_profiles.csv"
+)
+data1 = pandas.read_csv(
+    "/home/batuhansaylam/Desktop/hazine_ve_maliye_btgm_2023_staj/data/HDR21-22_Composite_indices_complete_time_series.csv"
+)
+data = data.merge(data1[["iso3", "country"]], how="inner", on="country")
+dash.register_page(__name__, path="/population")
+
+data2 = data[["country", "Region", "Population, male"]]
+data2 = data2.groupby(["Region"])["Population, male"].sum().reset_index()
+data2 = pandas.DataFrame(data2)
+data2["gender"] = ["male"] * len(data2)
+data2.columns = ["Region", "count", "gender"]
+data2 = data2.sort_values(by=["Region"])
+data3 = data[["country", "Region", "Population, female"]]
+data3 = data3.groupby(["Region"])["Population, female"].sum().reset_index()
+data3 = pandas.DataFrame(data3)
+data3["gender"] = ["female"] * len(data3)
+data3.columns = ["Region", "count", "gender"]
+data3 = data3.sort_values(by=["Region"])
+data2 = pandas.concat([data2, data3])
 data2.sort_values(by=["Region"])
-dicti={}
+dicti = {}
 for i in data2["Region"].unique():
-    data11=data2[data2["Region"]==i]
-    dicti[i]=data11[data11["gender"]=="female"].values[0,1]/data11[data11["gender"]=="male"].values[0,1]
-dicti=pandas.DataFrame.from_dict(dicti,orient="index").reset_index()
-dicti.columns=["Region","ratio"]
-data2=pandas.merge(data2,dicti,on="Region")
+    data11 = data2[data2["Region"] == i]
+    dicti[i] = (
+        data11[data11["gender"] == "female"].values[0, 1]
+        / data11[data11["gender"] == "male"].values[0, 1]
+    )
+dicti = pandas.DataFrame.from_dict(dicti, orient="index").reset_index()
+dicti.columns = ["Region", "ratio"]
+data2 = pandas.merge(data2, dicti, on="Region")
 data2
 
 fig1 = make_subplots(specs=[[{"secondary_y": True}]])
 
 fig1.add_trace(
-    go.Scatter(x=dicti['Region'], y=dicti['ratio'], mode="lines",name="Kadın nüfusun erkek nüfusa oranı"),
-    secondary_y=True
+    go.Scatter(
+        x=dicti["Region"],
+        y=dicti["ratio"],
+        mode="lines",
+        name="Kadın nüfusun erkek nüfusa oranı",
+    ),
+    secondary_y=True,
 )
 
 fig1.add_trace(
-    go.Bar(x=data2[data2["gender"]=="male"]['Region'], y=data2[data2["gender"]=="male"]['count'],name="Erkek nüfus"),
-    secondary_y=False
+    go.Bar(
+        x=data2[data2["gender"] == "male"]["Region"],
+        y=data2[data2["gender"] == "male"]["count"],
+        name="Erkek nüfus",
+    ),
+    secondary_y=False,
 )
 
 
 fig1.add_trace(
-    go.Bar(x=data2[data2["gender"]=="female"]["Region"],y=data2[data2["gender"]=="female"]["count"],name="Erkek nüfus")
+    go.Bar(
+        x=data2[data2["gender"] == "female"]["Region"],
+        y=data2[data2["gender"] == "female"]["count"],
+        name="Erkek nüfus",
+    )
 )
 
-fig1.update_layout(barmode='stack',
-                   xaxis=dict(title="Bölgeler"),
-                   yaxis=dict(title="Nüfus (milyon)"),
-                   yaxis2=dict(title="Kadın nüfusun erkek nüfusa oranı"))
+fig1.update_layout(
+    barmode="stack",
+    xaxis=dict(title="Bölgeler"),
+    yaxis=dict(title="Nüfus (milyon)"),
+    yaxis2=dict(title="Kadın nüfusun erkek nüfusa oranı"),
+)
 
 
-x=data["Education: Primary gross enrol. ratio (f/m per 100 pop.)"].tolist()
-f=[]
-m=[]
+x = data["Education: Primary gross enrol. ratio (f/m per 100 pop.)"].tolist()
+f = []
+m = []
 for i in x:
-    u=i.split("/")
+    u = i.split("/")
     f.append(float(u[0]))
-    if len(u) ==2:
+    if len(u) == 2:
         m.append(float(u[1]))
     else:
         m.append(0)
-data["Education: Primary gross enrol. ratio (f per 100 pop.)"]=f
-data["Education: Primary gross enrol. ratio (m per 100 pop.)"]=m
+data["Education: Primary gross enrol. ratio (f per 100 pop.)"] = f
+data["Education: Primary gross enrol. ratio (m per 100 pop.)"] = m
 
 
-x=data["Education: Secondary gross enrol. ratio (f/m per 100 pop.)"].tolist()
-f=[]
-m=[]
+x = data["Education: Secondary gross enrol. ratio (f/m per 100 pop.)"].tolist()
+f = []
+m = []
 for i in x:
-    u=i.split("/")
+    u = i.split("/")
     f.append(float(u[0]))
-    if len(u) ==2:
+    if len(u) == 2:
         m.append(float(u[1]))
     else:
         m.append(0)
-data["Education: Secondary gross enrol. ratio (f per 100 pop.)"]=f
-data["Education: Secondary gross enrol. ratio (m per 100 pop.)"]=m
+data["Education: Secondary gross enrol. ratio (f per 100 pop.)"] = f
+data["Education: Secondary gross enrol. ratio (m per 100 pop.)"] = m
 
 
-x=data["Education: Tertiary gross enrol. ratio (f/m per 100 pop.)"].tolist()
-f=[]
-m=[]
+x = data["Education: Tertiary gross enrol. ratio (f/m per 100 pop.)"].tolist()
+f = []
+m = []
 for i in x:
-    u=i.split("/")
+    u = i.split("/")
     if type(u[0]) == float:
         f.append(float(u[0]))
     else:
         f.append(0)
-    if len(u) ==2 and type(u[1]) == float:
+    if len(u) == 2 and type(u[1]) == float:
         m.append(float(u[1]))
     else:
         m.append(0)
-data["Education: Tertiary gross enrol. ratio (f per 100 pop.)"]=f
-data["Education: Tertiary gross enrol. ratio (m per 100 pop.)"]=m
-
+data["Education: Tertiary gross enrol. ratio (f per 100 pop.)"] = f
+data["Education: Tertiary gross enrol. ratio (m per 100 pop.)"] = m
 
 
 fig2 = go.Figure()
-fig2.add_trace(go.Scatter(x=data["Region"].unique(), y=data.groupby("Region")["Education: Primary gross enrol. ratio (f per 100 pop.)"].sum(), name='ilkokul (kadın)',
-                         line=dict(color='firebrick', width=4)))
-fig2.add_trace(go.Scatter(x=data["Region"].unique(), y=data.groupby("Region")["Education: Primary gross enrol. ratio (m per 100 pop.)"].sum(), name = 'ilkokul (erkek)',
-                         line=dict(color='royalblue', width=4)))
-fig2.add_trace(go.Scatter(x=data["Region"].unique(), y=data.groupby("Region")["Education: Secondary gross enrol. ratio (f per 100 pop.)"].sum(), name='Ortaokul (kadın)',
-                         line=dict(color='firebrick', width=4,
-                              dash='dash') # dash options include 'dash', 'dot', and 'dashdot'
-))
-fig2.add_trace(go.Scatter(x=data["Region"].unique(), y=data.groupby("Region")["Education: Secondary gross enrol. ratio (m per 100 pop.)"].sum(), name='Ortaokul (erkek)',
-                         line = dict(color='royalblue', width=4, dash='dash')))
-fig2.add_trace(go.Scatter(x=data["Region"].unique(), y=data.groupby("Region")["Education: Tertiary gross enrol. ratio (f per 100 pop.)"].sum(), name='Yüksek öğretim (kadın)',
-                         line = dict(color='firebrick', width=4, dash='dot')))
-fig2.add_trace(go.Scatter(x=data["Region"].unique(), y=data.groupby("Region")["Education: Tertiary gross enrol. ratio (m per 100 pop.)"].sum(), name='(YÜksek öğretim erkek)',
-                         line=dict(color='royalblue', width=4, dash='dot')))
+fig2.add_trace(
+    go.Scatter(
+        x=data["Region"].unique(),
+        y=data.groupby("Region")[
+            "Education: Primary gross enrol. ratio (f per 100 pop.)"
+        ].sum(),
+        name="ilkokul (kadın)",
+        line=dict(color="firebrick", width=4),
+    )
+)
+fig2.add_trace(
+    go.Scatter(
+        x=data["Region"].unique(),
+        y=data.groupby("Region")[
+            "Education: Primary gross enrol. ratio (m per 100 pop.)"
+        ].sum(),
+        name="ilkokul (erkek)",
+        line=dict(color="royalblue", width=4),
+    )
+)
+fig2.add_trace(
+    go.Scatter(
+        x=data["Region"].unique(),
+        y=data.groupby("Region")[
+            "Education: Secondary gross enrol. ratio (f per 100 pop.)"
+        ].sum(),
+        name="Ortaokul (kadın)",
+        line=dict(
+            color="firebrick", width=4, dash="dash"
+        ),  # dash options include 'dash', 'dot', and 'dashdot'
+    )
+)
+fig2.add_trace(
+    go.Scatter(
+        x=data["Region"].unique(),
+        y=data.groupby("Region")[
+            "Education: Secondary gross enrol. ratio (m per 100 pop.)"
+        ].sum(),
+        name="Ortaokul (erkek)",
+        line=dict(color="royalblue", width=4, dash="dash"),
+    )
+)
+fig2.add_trace(
+    go.Scatter(
+        x=data["Region"].unique(),
+        y=data.groupby("Region")[
+            "Education: Tertiary gross enrol. ratio (f per 100 pop.)"
+        ].sum(),
+        name="Yüksek öğretim (kadın)",
+        line=dict(color="firebrick", width=4, dash="dot"),
+    )
+)
+fig2.add_trace(
+    go.Scatter(
+        x=data["Region"].unique(),
+        y=data.groupby("Region")[
+            "Education: Tertiary gross enrol. ratio (m per 100 pop.)"
+        ].sum(),
+        name="(YÜksek öğretim erkek)",
+        line=dict(color="royalblue", width=4, dash="dot"),
+    )
+)
 
 # Edit the layout
-fig2.update_layout(title='Bölgelere göre eğitim seviyeleri',
-                   xaxis_title='Bölgeler',
-                   yaxis_title='Eğitim:  kayıtlı oranı (100 kişi başına bir öğrenci)')
-
-
-
+fig2.update_layout(
+    title="Bölgelere göre eğitim seviyeleri",
+    xaxis_title="Bölgeler",
+    yaxis_title="Eğitim:  kayıtlı oranı (100 kişi başına bir öğrenci)",
+)
 
 
 fig3 = go.Figure()
 
-fig3.add_trace(go.Scatter(
-    x=data["Region"].unique(),
-    y=data.groupby("Region")["Population in thousands (2017)"].sum(),
-    name="Nüfus verisi"
-))
+fig3.add_trace(
+    go.Scatter(
+        x=data["Region"].unique(),
+        y=data.groupby("Region")["Population in thousands (2017)"].sum(),
+        name="Nüfus verisi",
+    )
+)
 
 
-fig3.add_trace(go.Scatter(
-    x=data["Region"].unique(),
-    y=data.groupby("Region")["Fertility rate, total (live births per woman)"].sum(),
-    name="Doğum oranı, toplam (kadın başına canlı doğum )",
-    yaxis="y2"
-))
-
+fig3.add_trace(
+    go.Scatter(
+        x=data["Region"].unique(),
+        y=data.groupby("Region")["Fertility rate, total (live births per woman)"].sum(),
+        name="Doğum oranı, toplam (kadın başına canlı doğum )",
+        yaxis="y2",
+    )
+)
 
 
 # Create axis objects
 fig3.update_layout(
-    xaxis=dict(
-        domain=[0.3, 0.7]
-    ),
-    yaxis=dict(
-        title="Nüfus (Bin) (2017)"
-
-    ),
+    xaxis=dict(domain=[0.3, 0.7]),
+    yaxis=dict(title="Nüfus (Bin) (2017)"),
     yaxis2=dict(
         title="Doğum oranı, toplam (kadın başına canlı doğum ) ",
-        tickfont=dict(
-            color="#ff7f0e"
-        ),
+        tickfont=dict(color="#ff7f0e"),
         anchor="x",
         overlaying="y",
         side="right",
-        position=0.15
-    )
+        position=0.15,
+    ),
 )
 
 # Update layout properties
@@ -178,39 +243,69 @@ fig3.update_layout(
     width=800,
 )
 
-layout=html.Div([
-    html.Div([
-        "Bölge/Bölgeler seç:",
-        dbc.Checklist(options=['SouthAmerica', 'Oceania', 'WesternEurope', 'EasternEurope','SouthernEurope', 'NorthernAmerica', 'EasternAsia', 'WesternAsia','NorthernEurope', 'NorthernAfrica', 'SouthernAsia','South-easternAsia', 'CentralAmerica', 'SouthernAfrica']
-,id="memory",inline=True),
-        dcc.Graph(id="pop-traffic")
-    ]),
-    html.Div([
-        dcc.Graph(figure=fig1),
-        "En çok kadın ve erkek nüfuzu Güney Asya'da olsa da kadın nüfusun erkeğe göre en fazla olduğu bölge Doğu Avrupa. En az erkek ve kadın nüfusunun olduğu bölge ise Okyanusya olurken kadın nüfusunun erkeğe göre en az olduğu bölge Batı Asya."
-    ]),
-    html.Div([
-        dcc.Graph(figure=fig2),
-        "En yüksek kadın ve erkek ilkokul ve yüksek öğretim eğitimleri Batı Avrupa, orta okul eğitimleri Kuzey Amerika bölgelerindedir. Diğer bir yandan, üç eğitim seviyesi için de en düşük eğitim nüfuzuna sahip ülkeler Kuzey Afrika bölgesindedir. "
-    ]),
-    html.Div([
-        dcc.Graph(figure=fig3),
-        "En yüksek doğum oranı Orta Amerika bölgesinde olmasına rağmen en fazla nüfusa sahip ülkeler Güney Asya'dadır. En düşük nüfus ise Güney Avrupa'da olmasına rağmen en düşük doğum oranı da Doğu Asya'dadır."
-    ])
-])
-
-@callback(
-    Output("pop-traffic","figure"),
-    [Input("memory","value")]
+layout = html.Div(
+    [
+        html.Div(
+            [
+                "Bölge/Bölgeler seç:",
+                dbc.Checklist(
+                    options=[
+                        "SouthAmerica",
+                        "Oceania",
+                        "WesternEurope",
+                        "EasternEurope",
+                        "SouthernEurope",
+                        "NorthernAmerica",
+                        "EasternAsia",
+                        "WesternAsia",
+                        "NorthernEurope",
+                        "NorthernAfrica",
+                        "SouthernAsia",
+                        "South-easternAsia",
+                        "CentralAmerica",
+                        "SouthernAfrica",
+                    ],
+                    id="memory",
+                    inline=True,
+                    value=["WesternEurope"],
+                ),
+                dcc.Graph(id="pop-traffic"),
+            ]
+        ),
+        html.Div(
+            [
+                dcc.Graph(figure=fig1),
+                "En çok kadın ve erkek nüfuzu Güney Asya'da olsa da kadın nüfusun erkeğe göre en fazla olduğu bölge Doğu Avrupa. En az erkek ve kadın nüfusunun olduğu bölge ise Okyanusya olurken kadın nüfusunun erkeğe göre en az olduğu bölge Batı Asya.",
+            ]
+        ),
+        html.Div(
+            [
+                dcc.Graph(figure=fig2),
+                "En yüksek kadın ve erkek ilkokul ve yüksek öğretim eğitimleri Batı Avrupa, orta okul eğitimleri Kuzey Amerika bölgelerindedir. Diğer bir yandan, üç eğitim seviyesi için de en düşük eğitim nüfuzuna sahip ülkeler Kuzey Afrika bölgesindedir. ",
+            ]
+        ),
+        html.Div(
+            [
+                dcc.Graph(figure=fig3),
+                "En yüksek doğum oranı Orta Amerika bölgesinde olmasına rağmen en fazla nüfusa sahip ülkeler Güney Asya'dadır. En düşük nüfus ise Güney Avrupa'da olmasına rağmen en düşük doğum oranı da Doğu Asya'dadır.",
+            ]
+        ),
+    ]
 )
+
+
+@callback(Output("pop-traffic", "figure"), [Input("memory", "value")])
 def scline(reg):
-    data1=pandas.DataFrame(columns=data.columns.tolist())
+    data1 = pandas.DataFrame(columns=data.columns.tolist())
     for i in reg:
-        data1=data1.append(data[data["Region"]==i])
-    fig = px.scatter(data1, x="Population density (per km2, 2017)", y="Traffic commute time index", marginal_x="box", marginal_y="violin",color="Region")
-    fig.update_traces(showlegend=True) #trendlines have showlegend=False by default
-    return fig 
-
-
-
-
+        data1 = data1._append(data[data["Region"] == i])
+    fig = px.scatter(
+        data1,
+        x="Population density (per km2, 2017)",
+        y="Traffic commute time index",
+        marginal_x="box",
+        marginal_y="violin",
+        color="Region",
+    )
+    fig.update_traces(showlegend=True)  # trendlines have showlegend=False by default
+    return fig
--- pages/Ticaret.py
+++ pages/Ticaret.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html, dcc,Input,Output,callback
+from dash import html, dcc, Input, Output, callback
 import pandas
 import plotly.express as px
 import plotly.graph_objects as go
@@ -10,34 +10,56 @@ import numpy as np
 import plotly.figure_factory as ff
 from sklearn.linear_model import LinearRegression
 from plotly.subplots import make_subplots
-import statsmodels.api as sm 
+import statsmodels.api as sm
 
 load_figure_template("slate")
-data=pandas.read_csv("/home/batuhansaylam/Desktop/hazine_ve_maliye_btgm_2023_staj/data/soci_econ_country_profiles.csv")
-data1=pandas.read_csv("/home/batuhansaylam/Desktop/hazine_ve_maliye_btgm_2023_staj/data/HDR21-22_Composite_indices_complete_time_series.csv")
-data=data.merge(data1[["iso3","country"]],how="inner",on="country")
-dash.register_page(__name__, path='/trading')
-fig = make_subplots(rows=1, cols=2, specs=[[{}, {}]], shared_xaxes=True,
-                    shared_yaxes=False, vertical_spacing=0.001)
-fig.append_trace(go.Bar(
-    y=data["Region"].unique(),
-    x=data.groupby("Region")["Military expenditure (% of GDP)"].sum(),
-    marker_color='indianred',orientation='h',name="Toplam savunma harcaması (% GDP)"
-),1,1)
+data = pandas.read_csv(
+    "/home/batuhansaylam/Desktop/hazine_ve_maliye_btgm_2023_staj/data/soci_econ_country_profiles.csv"
+)
+data1 = pandas.read_csv(
+    "/home/batuhansaylam/Desktop/hazine_ve_maliye_btgm_2023_staj/data/HDR21-22_Composite_indices_complete_time_series.csv"
+)
+data = data.merge(data1[["iso3", "country"]], how="inner", on="country")
+dash.register_page(__name__, path="/trading")
+fig = make_subplots(
+    rows=1,
+    cols=2,
+    specs=[[{}, {}]],
+    shared_xaxes=True,
+    shared_yaxes=False,
+    vertical_spacing=0.001,
+)
+fig.append_trace(
+    go.Bar(
+        y=data["Region"].unique(),
+        x=data.groupby("Region")["Military expenditure (% of GDP)"].sum(),
+        marker_color="indianred",
+        orientation="h",
+        name="Toplam savunma harcaması (% GDP)",
+    ),
+    1,
+    1,
+)
 fig.add_trace(
     go.Bar(
-
         y=data["Region"].unique(),
         x=data.groupby("Region")["Health: Total expenditure (% of GDP)"].sum(),
-        marker_color='rgba(246, 78, 139, 0.6)',orientation='h',name="Toplam sağlık harcaması (% GDP)"
+        marker_color="rgba(246, 78, 139, 0.6)",
+        orientation="h",
+        name="Toplam sağlık harcaması (% GDP)",
     )
 )
 
 
-fig.append_trace(go.Line(
-    y=data["Region"].unique(),
-    x=data.groupby("Region")["Tax revenue (% of GDP)"].sum(),name="Vergi geliri (% GDP)"
-),1,2)
+fig.append_trace(
+    go.Line(
+        y=data["Region"].unique(),
+        x=data.groupby("Region")["Tax revenue (% of GDP)"].sum(),
+        name="Vergi geliri (% GDP)",
+    ),
+    1,
+    2,
+)
 fig.update_layout(
     barmode="stack",
     yaxis=dict(
@@ -51,7 +73,7 @@ fig.update_layout(
         showgrid=False,
         showline=True,
         showticklabels=False,
-        linecolor='rgba(102, 102, 102, 0.8)',
+        linecolor="rgba(102, 102, 102, 0.8)",
         linewidth=2,
         domain=[0, 0.85],
     ),
@@ -70,79 +92,124 @@ fig.update_layout(
         showticklabels=True,
         showgrid=True,
         domain=[0.47, 1],
-        side='top',
+        side="top",
         dtick=25000,
     ),
     legend=dict(x=0.029, y=1.038, font_size=10),
     margin=dict(l=100, r=20, t=70, b=70),
-    paper_bgcolor='rgb(248, 248, 255)',
-    plot_bgcolor='rgb(248, 248, 255)',
+    paper_bgcolor="rgb(248, 248, 255)",
+    plot_bgcolor="rgb(248, 248, 255)",
 )
-data["Economy: Agriculture (% of GVA)"] =  pandas.to_numeric(data["Economy: Agriculture (% of GVA)"], errors='coerce')
-data=data.groupby("Region")[["Economy: Agriculture (% of GVA)","Economy: Industry (% of GVA)","Economy: Services and other activity (% of GVA)"]].sum().reset_index()
+data["Economy: Agriculture (% of GVA)"] = pandas.to_numeric(
+    data["Economy: Agriculture (% of GVA)"], errors="coerce"
+)
+data = (
+    data.groupby("Region")[
+        [
+            "Economy: Agriculture (% of GVA)",
+            "Economy: Industry (% of GVA)",
+            "Economy: Services and other activity (% of GVA)",
+        ]
+    ]
+    .sum()
+    .reset_index()
+)
 
-x=data[["Economy: Agriculture (% of GVA)","Economy: Industry (% of GVA)","Economy: Services and other activity (% of GVA)"]].sum(axis=1).tolist()
-data["total"]=x
-data.iloc[:, 1:] = data.iloc[:, 1:].div(data['total'], axis=0).mul(100).round(2)
+x = (
+    data[
+        [
+            "Economy: Agriculture (% of GVA)",
+            "Economy: Industry (% of GVA)",
+            "Economy: Services and other activity (% of GVA)",
+        ]
+    ]
+    .sum(axis=1)
+    .tolist()
+)
+data["total"] = x
+data.iloc[:, 1:] = data.iloc[:, 1:].div(data["total"], axis=0).mul(100).round(2)
 
 
+fig1 = go.Figure()
 
-
-fig1= go.Figure()
-
-fig1.add_trace(go.Bar(
-    x=data["Economy: Agriculture (% of GVA)"], y=data["Region"].unique(),
-    orientation='h',
-    marker=dict(
-        color='rgba(38, 24, 74, 0.8)',
-        line=dict(color='rgb(248, 248, 249)')
-        ),name="Tarım brüt katma değeri"
-))
-fig1.add_trace(go.Bar(
-    x=data["Economy: Industry (% of GVA)"], y=data["Region"].unique(),
-    orientation='h',
-    marker=dict(
-        line=dict(color='rgb(248, 248, 249)')
-        ),name="Sanayi brüt katma değeri"
-))
-fig1.add_trace(go.Bar(
-    x=data["Economy: Services and other activity (% of GVA)"], y=data["Region"].unique(),
-    orientation='h',
-    marker=dict(
-        line=dict(color='rgb(248, 248, 249)')
-        ),name="Hizmet ve diğer sektörlerin brüt katma değeri"
-))
+fig1.add_trace(
+    go.Bar(
+        x=data["Economy: Agriculture (% of GVA)"],
+        y=data["Region"].unique(),
+        orientation="h",
+        marker=dict(
+            color="rgba(38, 24, 74, 0.8)", line=dict(color="rgb(248, 248, 249)")
+        ),
+        name="Tarım brüt katma değeri",
+    )
+)
+fig1.add_trace(
+    go.Bar(
+        x=data["Economy: Industry (% of GVA)"],
+        y=data["Region"].unique(),
+        orientation="h",
+        marker=dict(line=dict(color="rgb(248, 248, 249)")),
+        name="Sanayi brüt katma değeri",
+    )
+)
+fig1.add_trace(
+    go.Bar(
+        x=data["Economy: Services and other activity (% of GVA)"],
+        y=data["Region"].unique(),
+        orientation="h",
+        marker=dict(line=dict(color="rgb(248, 248, 249)")),
+        name="Hizmet ve diğer sektörlerin brüt katma değeri",
+    )
+)
 fig1.update_layout(
     xaxis_title="Toplam brüt katma değerdeki pay",
     yaxis_title="Bölgeler",
-    barmode='stack')
+    barmode="stack",
+)
 
 
-data=pandas.read_csv("/home/batuhansaylam/Desktop/hazine_ve_maliye_btgm_2023_staj/data/soci_econ_country_profiles.csv")
+data = pandas.read_csv(
+    "/home/batuhansaylam/Desktop/hazine_ve_maliye_btgm_2023_staj/data/soci_econ_country_profiles.csv"
+)
 
-fig3 = px.treemap(data, path=[px.Constant("world"),"Region","country"], values="International trade: Exports (million US$)",
-                  color="International trade: Imports (million US$)",
-                  color_continuous_scale='RdBu',
-                  color_continuous_midpoint=np.average(data["International trade: Imports (million US$)"]))
-fig3.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+fig3 = px.treemap(
+    data,
+    path=[px.Constant("world"), "Region", "country"],
+    values="International trade: Exports (million US$)",
+    color="International trade: Imports (million US$)",
+    color_continuous_scale="RdBu",
+    color_continuous_midpoint=np.average(
+        data["International trade: Imports (million US$)"]
+    ),
+)
+fig3.update_layout(margin=dict(t=50, l=25, r=25, b=25))
 
-layout=html.Div([
-    html.Div([
-        html.Div(["Government Expend-Tex revenue"]),
-        dcc.Graph(figure=fig),
-        "En yüksek sağlık harcaması Güney Asya ve en yüksek askeri harcama ise Orta Amerika bölgesindedir. En düşük askeri harcama Kuzey Afrika'da iken en düşük sağlık harcaması Doğu Avrupa'dadır.Ek olarak en çok vergi toplayan ülkeler Kuzey Amerika'da ve en düşük vergi toplayan ülkeler ise Güney Amerika'dadır."
-    ]),
-    html.Div([
-        html.Div([
-            html.Div(["the percent of econ"]),
-            dcc.Graph(figure=fig1),
-            "Ekonomisi içinde Tarım sektörü en çok yere sahip olan ülkeler Güney Asyada iken en düşüş paya sahip olan ülkeler Kuzey Amerika'dadır. Ekonomisi içinde en büyük paya sanayisi sahip olan ülkeler Kuzey Afrika'da iken en düşük paya sahip Batı Avrupa'dadır. Ekonomisi içinde hizmet ve diğer sektörler tarafından en büyük paya sahip olan ülkeler Batı Avrupa'da iken en düşük paya sahip ülkeler Güney Doğu Asya'dır. "
-    ]),
-    html.Div([
-        "İhracat-ithalat şeması:",
-        dcc.Graph(figure=fig3),
-        "En fazla ihracat yapan ülke ABD olmasına rağmen en yüksek ihracat yapan ülke Çin'dir. En düşük ithalat ve ihracat yapan ülke ise Kıbrıs'dır. "
-    ])
-    ])
-
-])
+layout = html.Div(
+    [
+        html.Div(
+            [
+                html.Div(["Government Expend-Tex revenue"]),
+                dcc.Graph(figure=fig),
+                "En yüksek sağlık harcaması Güney Asya ve en yüksek askeri harcama ise Orta Amerika bölgesindedir. En düşük askeri harcama Kuzey Afrika'da iken en düşük sağlık harcaması Doğu Avrupa'dadır.Ek olarak en çok vergi toplayan ülkeler Kuzey Amerika'da ve en düşük vergi toplayan ülkeler ise Güney Amerika'dadır.",
+            ]
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.Div(["the percent of econ"]),
+                        dcc.Graph(figure=fig1),
+                        "Ekonomisi içinde Tarım sektörü en çok yere sahip olan ülkeler Güney Asyada iken en düşüş paya sahip olan ülkeler Kuzey Amerika'dadır. Ekonomisi içinde en büyük paya sanayisi sahip olan ülkeler Kuzey Afrika'da iken en düşük paya sahip Batı Avrupa'dadır. Ekonomisi içinde hizmet ve diğer sektörler tarafından en büyük paya sahip olan ülkeler Batı Avrupa'da iken en düşük paya sahip ülkeler Güney Doğu Asya'dır. ",
+                    ]
+                ),
+                html.Div(
+                    [
+                        "İhracat-ithalat şeması:",
+                        dcc.Graph(figure=fig3),
+                        "En fazla ihracat yapan ülke ABD olmasına rağmen en yüksek ihracat yapan ülke Çin'dir. En düşük ithalat ve ihracat yapan ülke ise Kıbrıs'dır. ",
+                    ]
+                ),
+            ]
+        ),
+    ]
+)
